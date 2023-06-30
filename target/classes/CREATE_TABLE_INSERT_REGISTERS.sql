cd402c78ad0552578ccb8f3baee3e69f8a06a965
Felipe Rocha de Oliveira <felipe.oliveira200085@gmail.com>
2023-06-30 14:41:06 -0300

    Inclusão de novos registros iniciais no banco

--- target/classes/CREATE_TABLE_INSERT_REGISTERS.sql
+++ target/classes/CREATE_TABLE_INSERT_REGISTERS.sql
@@ -1,3 +1,3 @@
 CREATE TABLE convidados (rowid bigint auto_increment, nm_convidado VARCHAR(100), qnt_convidados int);
 
-INSERT INTO convidados (nm_convidado, qnt_convidados) VALUES ('Fernando', 2); 
+INSERT INTO convidados (nm_convidado, qnt_convidados) VALUES ('Fernando', 2), ('Ricardo', 4), ('Maria', 1), ('Carla', 0); 
